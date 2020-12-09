8a12316caae908ee6aa0b26ed4f147b6988b3d45
geeksKK8 <3180104180@zju.edu.cn>
2020-12-09 23:26:01 +0800

    针对客户端更改，进行适配

--- server.cpp
+++ server.cpp
@@ -21,9 +21,10 @@ void Server::incomingConnection(qintptr socketDescriptor)
     TcpClientSocket *tcpclientsocket = new TcpClientSocket(this);//只要有新的连接就生成一个新的通信套接字
     //将新创建的通信套接字描述符指定为参数socketdescriptor
     tcpclientsocket->setSocketDescriptor(socketDescriptor);
-
+    qDebug()<<"descriptor:"<<socketDescriptor;
     //将这个套接字加入客户端套接字列表中
     tcpclientsocketlist.append(tcpclientsocket);
+
 
 
     //接收到tcpclientsocket发送过来的更新界面的信号
@@ -36,8 +37,32 @@ void Server::sliotupdateserver(QString msg, int length)
 {
     //将这个信号发送给界面
     emit updateserver(msg, length);
-
     //将收到的信息发送给每个客户端,从套接字列表中找到需要接收的套接字
+
+    if(msg[0]=="2"){
+        qDebug()<<"to one";
+        QString remain = msg.mid(1);
+        QString ptr = remain.split(":")[0];
+        for(int i = 0; i < tcpclientsocketlist.count(); i++)
+        {
+
+            QTcpSocket *item = tcpclientsocketlist.at(i);
+            QString *name = usernamelist.at(i);
+    //        if(item->write((char*)msg.toUtf8().data(), length) != length)
+    //        {
+    //            continue;
+    //        }
+            if(*name == ptr)
+                item->write(msg.toUtf8().data());
+        }
+    }
+    if(msg[0]=="0"){
+        QString remain = msg.mid(1);
+        QString ptr = remain.split(":")[0];
+        usernamelist.append(&ptr);
+    }
+    if(msg[0] != "2"){
+        qDebug()<<"to all";
     for(int i = 0; i < tcpclientsocketlist.count(); i++)
     {
         QTcpSocket *item = tcpclientsocketlist.at(i);
@@ -45,7 +70,9 @@ void Server::sliotupdateserver(QString msg, int length)
 //        {
 //            continue;
 //        }
+
         item->write(msg.toUtf8().data());
+    }
     }
 
 }
